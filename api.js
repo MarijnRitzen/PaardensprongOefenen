0ec29475dcf896210b8d2fe51f900ef2e0e0295a
Marijn Ritzen <marijnritzen@outlook.com>
2021-03-07 13:50:08 +0100

    lil change again

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ let port = 80;
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/resources/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 app.listen(process.env.PORT || port, () => console.log(`Listening on ${port}`))
